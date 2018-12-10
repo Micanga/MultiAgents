a35934d0eb447e6728eba7e56550a7a5b5da7572
Micanga <matheus.aparecido.alves@usp.br>
2018-12-10 14:06:40 +0000

    update: scenario generator and scripts

--- po_scenario_generator.py
+++ po_scenario_generator.py
@@ -9,23 +9,21 @@ from numpy import pi
 
 # (1) CONFIG.CSV - INFORMATION
 # Defining the parameter estimation modes and
-# the mcts modes
-parameter_estimation_modes = ['ABU','AGA','MIN']
-mcts_modes = ['UCT','UCTH']
 max_depth_set = ['50']
 iteration_max_set = ['50']
 
 # (2) SIM.CSV - INFORMATION
 # Defining the parameter of simulation file
-square_grid_size 	= [10]
-number_of_agents 	= [1]
-number_of_items		= [10]
 possible_directions = ['N','S','E','W']
-agent_types 		= ['l1','l2'] 
+agent_types 		= ['l1','l2','f1','f2']
+selected_types 		= [False,False]
 
-def create_config_file(current_folder,parameter_estimation_mode,mcts_mode,train_mode):
+def random_pick(set_):
+	return set_[randint(0,len(set_)-1)]
 
-	filename = current_folder + 'po_config.csv'
+def write_config_file(current_folder,parameter_estimation_mode,mcts_mode,train_mode):
+
+	filename = current_folder + 'poconfig.csv'
 	with open(filename, 'wb+') as file:
 		writer = csv.writer(file, delimiter=',')
 		GRID = ['type_selection_mode', 'AS']
@@ -35,12 +33,12 @@ def create_config_file(current_folder,parameter_estimation_mode,mcts_mode,train_
 		writer.writerows([['generated_data_number', '100']])
 		writer.writerows([['reuseTree','False']])
 		writer.writerows([['mcts_mode', mcts_mode]])
-		writer.writerows([['PF_add_threshold', '0.8']])
+		writer.writerows([['PF_add_threshold', '0.9']])
 		writer.writerows([['PF_del_threshold', '0.9']])
 		writer.writerows([['PF_weight', '1.2']])
-		writer.writerows([['iteration_max', iteration_max_set[randint(0,len(iteration_max_set)-1)]]])
-		writer.writerows([['max_depth', max_depth_set[randint(0,len(max_depth_set)-1)]]])
-		writer.writerows([['sim_path', 'posim.csv']])
+		writer.writerows([['iteration_max', iteration_max_set[0]]])
+		writer.writerows([['max_depth', max_depth_set[0]]])
+		writer.writerows([['sim_path', 'sim.csv']])
 
 
 def generateRandomNumber (grid,gridValue):
@@ -52,101 +50,118 @@ def generateRandomNumber (grid,gridValue):
 			grid[testXValue][testYValue] = 1
 			return testXValue,testYValue,grid
 
+def selectType():
+	global agent_types, selected_types
+
+	# 1. Selecting a ramdom type
+	agentType = choice(agent_types)
+
+	# 2. Verifing if is able to generate this type
+	# follower 1 needs lider 1
+	if agentType == 'f1' and selected_types[0] == False:
+		while agentType != 'l1' or agentType != 'l2':
+			agentType = choice(possible_directions)
+		if agentType == 'l1':
+			selected_types[0] = True
+		else:
+			selected_types[1] = True
+	# follower 2 needs lider 2
+	if agentType == 'f2' and selected_types[1] == False:
+		while agentType != 'l1' or agentType != 'l2':
+			agentType = choice(possible_directions)
+		if agentType == 'l1':
+			selected_types[0] = True
+		else:
+			selected_types[1] = True
+	# lider 1 and 2 as Selected Type
+	if agentType == 'l1':
+		selected_types[0] = True
+	else:
+		selected_types[1] = True
+
+	return agentType
 
 def main():
 	# 0. Checking the terminal input
-	if len(sys.argv) != 3:
-		print 'usage: python po_run_world.py [main_radius] [angle]'
+	if len(sys.argv) != 7:
+		print 'usage: python po_scenario_generator.py [experiment] [size] [nagents] [nitems] [radius] [angle]'
 		exit(0)
 
 	# 1. Taking the information
-	MAIN_INFO = sys.argv[1] + '_' + sys.argv[2]
-	LEVEL = 1
-	RADIUS = int(sys.argv[1])
-	if sys.argv[2] == '0pi':
-		ANGLE = 0
-	elif sys.argv[2] == '1pi2':
-		ANGLE = (1/2)*pi
-	elif sys.argv[2] == '1pi':
-		ANGLE = pi
-	elif sys.argv[2] == '3pi2':
-		ANGLE = (3/2)*pi
-	elif sys.argv[2] == '2pi':
-		ANGLE = 2*pi
-	else:
-		exit(1)
+	experiment = argv[1]
+	size = int(sys.argv[2])
+	nagents = int(sys.argv[3])
+	nitems = int(sys.argv[4])
+	radius = int(sys.argv[5])
+	angle = float(sys.argv[6])
 
 	# 1. Creating the possible configuration files
-	for parameter_estimation_mode in parameter_estimation_modes:
-		# a. choosing the parameter estimation mode
-		if parameter_estimation_mode == 'MIN':
-			train_mode= 'history_based'
-		else:
-			train_mode= 'none_history_based'
+	# a. choosing the parameter estimation mode
+	if experiment == 'MIN':
+		train_mode = 'history_based'
+	else:
+		train_mode = 'none_history_based'
 
-		# b. choosing the mcts mode
-		for mcts_mode in mcts_modes:
-			if mcts_mode == 'UCT' :
-				MC_type = 'M'
-			else:
-				MC_type = 'O'
-			sub_dir = 'PO_'+ MC_type + '_' + parameter_estimation_mode
-			current_folder = "po_inputs/" + sub_dir + '/'
+	# b. choosing the mcts mode
+	mcts_mode = 'UCTH'
+	MC_type = 'O'
 
-			if not os.path.exists(current_folder):
-					os.mkdir(current_folder, 0755)
+	# c. creating the necessary folder
+	sub_dir = 'PO_'+ MC_type + '_' + experiment
+	current_folder = "po_inputs/" + sub_dir + '/'
+	if not os.path.exists(current_folder):
+		os.mkdir(current_folder, 0755)
 
-			# c. creating the config files
-			create_config_file(current_folder, parameter_estimation_mode, mcts_mode,train_mode)
+	# d. creating the config files
+	create_config_file(current_folder, experiment, mcts_mode,train_mode)
 
 	# 2. Creating the files
-	global MAIN_INFO,LEVEL, RADIUS, ANGLE
-	for path,all_sub_dir,files in os.walk("po_inputs/"):
-		for cur_dir in all_sub_dir:
-			print all_sub_dir
-			# a. setting the file name
-			filename = path + cur_dir + '/' + 'posim.csv'
-			print filename
+	# a. setting the file name
+	filename = current_folder + '/' + 'posim.csv'
+	print filename
 
-			# b. creating the a csv file
-			with open(filename,'wb+') as file:
-				writer = csv.writer(file,delimiter = ',')
+	# b. creating the a csv file
+	with open(filename,'wb+') as file:
+		writer = csv.writer(file,delimiter = ',')
 
-				# c. choosing the grid size
-				grid_size = square_grid_size[randint(0,len(square_grid_size)-1)]
-				grid = [[0 for col in range(grid_size)] for row in range(grid_size)]
-				GRID = ['grid',grid_size,grid_size]
-				writer.writerows([GRID])
+		# c. choosing the grid size
+		grid_size = size
+		grid = [[0 for col in range(grid_size)] for row in range(grid_size)]
 
-				# d. defining the main agent parameters
-				mainx,mainy,grid = generateRandomNumber(grid,grid_size)
-				mainDirection    = choice(possible_directions)
-				mainType  = 'm'
-				mainLevel = LEVEL
-				mainRadius, mainAngle = RADIUS, ANGLE
-				MAIN = ['main',mainx,mainy,mainDirection,mainType,mainLevel,mainRadius,mainAngle]
-				writer.writerows([MAIN])
+		GRID = ['grid',grid_size,grid_size]
+		writer.writerows([GRID])
 
-				# e. defining the commum agents
-				nagents = 1 #number_of_agents[randint(0,len(number_of_agents)-1)]
-				for agent_idx in range(nagents):
-					agentx,agenty,grid = generateRandomNumber(grid,grid_size)
-					agentDirection = choice(possible_directions)
-					agentType = 'l1'
-					agentLevel = round(random.uniform(0,1), 3)
-					agentRadius = round(random.uniform(0.1,1), 3)
-					agentAngle = round(random.uniform(0.1,1), 3)
+		# d. defining the main agent parameters
+		mainx,mainy,grid = generateRandomNumber(grid,grid_size)
+		mainDirection    = choice(possible_directions)
+		mainType  = 'm'
+		mainLevel = 1
+		mainRadius, mainAngle = radius, angle
 
-					AGENT = ['agent'+ str(agent_idx),agentx,agenty,agentDirection,agentType,agentLevel,agentRadius,agentAngle]
-					writer.writerows([AGENT])
+		MAIN = ['main',mainx,mainy,mainDirection,mainType,mainLevel,mainRadius,mainAngle]
+		writer.writerows([MAIN])
 
-				nitems = 10 #number_of_items[randint(0,len(number_of_items)-1)]
-				for item_idx in range(nitems):
-					itemx,itemy,grid = generateRandomNumber(grid,grid_size)
-					itemLevel = round(random.uniform(0,1), 3)
+		# e. defining the commum agents
+		nagents = nagents
+		for agent_idx in range(nagents):
+			agentx,agenty,grid = generateRandomNumber(grid,grid_size)
+			agentDirection = choice(possible_directions)
+			agentType = selectType()
+			agentLevel = round(random.uniform(0,1), 3)
+			agentRadius = round(random.uniform(0.1,1), 3)
+			agentAngle = round(random.uniform(0.1,1), 3)
 
-					ITEM = ['item'+ str(item_idx),itemx,itemy,itemLevel]
-					writer.writerows([ITEM])
+			AGENT = ['agent'+ str(agent_idx),agentx,agenty,agentDirection,agentType,agentLevel,agentRadius,agentAngle]
+			writer.writerows([AGENT])
 
+		nitems = nitems
+		for item_idx in range(nitems):
+			itemx,itemy,grid = generateRandomNumber(grid,grid_size)
+			itemLevel = round(random.uniform(0,1), 3)
+
+			ITEM = ['item'+ str(item_idx),itemx,itemy,itemLevel]
+			writer.writerows([ITEM])
+
+	return current_folder
 if __name__ == '__main__':
     main()
---
+++ script.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+import os
+import sys
+import subprocess
+import time
+
+from numpy import pi
+
+# 0. General Settings
+number_of_tests = 100
+
+square_grid_size    = ['10','15','20','25']
+number_of_agents    = ['1','2','3','4','5']
+number_of_items     = ['10','15','20','25']
+
+# 1. Defining the experiment type
+experiment_type_set = ['ABU', 'AGA', 'MIN']
+
+# 2. Starting the experiment
+test_number = 0
+while test_number < number_of_tests:
+    for experiment in experiment_type_set:
+        for size in square_grid_size:
+            for nagents in number_of_agents:
+                for nitems in number_of_items:
+                    print '----- STARTING TEST ',test_number,' -----'
+                    print '| Experiment: ',experiment
+                    print '| Size: ',size
+                    print '| N Agents: ',nagents
+                    print '| N Items: ',nitems
+                    print '---------'
+
+                    # a. generating random scenarios
+                    print '- Generating Scenario'
+                    scenario_generator = 'python scenario_generator.py ' +\
+                        experiment + ' ' + size + ' ' + nagents + ' '
+                    experiment_dir = os.system(scenario_generator)
+                    time.sleep(1)
+
+                    # b. openning the target dir
+                    print '- Starting the process'
+                    filename = 'sim.csv'
+                    experiment = 'python run_world.py '+ experiment_dir + ' ' + filename
+
+                    print experiment
+                    os.system(experiment)
+                    time.sleep(5)
+
+    test_number += 1
